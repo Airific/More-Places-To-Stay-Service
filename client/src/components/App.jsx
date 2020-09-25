6188a14eac122a07e011765554118d623fa0e382
Karl Mabunga <karlmabunga@yahoo.com>
2020-09-25 10:20:14 -0700

    Set up webpack and the folders for distribution and source. Created an index.html is the distribution and successfully can render a 'Hello World' from react.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+export default App;
